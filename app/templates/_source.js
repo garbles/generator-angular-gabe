a75b1cc4a19c9f4d31123015aa471649aac95fc5
Gabriel Scholz <gabe.scholz@zozi.com>
2014-06-10 09:04:28 -0700

    Add support for CommonJS/Browserify

--- app/templates/_source.js
+++ app/templates/_source.js
@@ -1,5 +1,8 @@
-(function(window, angular, undefined) {'use strict';
+if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports){
+  module.exports = '<%= namespacedPackageName %>';
+}
 
+(function(window, angular, undefined) {'use strict';
 angular.module('<%= namespacedPackageName %>', [])
 
 })(window, window.angular);
